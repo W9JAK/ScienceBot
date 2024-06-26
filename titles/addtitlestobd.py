6557cfa2c53cf7f8898c74183a26e5c407ad50e6
W9JAK <112979587+W9JAK@users.noreply.github.com>
2024-06-26 21:18:34 +0300

    fix

--- titles/addtitlestobd.py
+++ titles/addtitlestobd.py
@@ -1,11 +1,21 @@
+from fileprocess import process_file
 from db import create_table, insert_job_titles, delete_all_job_titles
 
 if __name__ == '__main__':
-    with open('code/titles/processed_job_titles.txt', 'r', encoding='utf-8') as f:
+    file_path = 'root/titles/titles.txt'
+    processed_file_path = 'root/titles/processed_job_titles.txt'
+
+    job_titles = process_file(file_path)
+
+    with open(processed_file_path, 'w', encoding='utf-8') as f:
+        for title in job_titles:
+            f.write(title + '\n')
+
+    with open(processed_file_path, 'r', encoding='utf-8') as f:
         job_titles = [line.strip() for line in f.readlines()]
 
     create_table()
     delete_all_job_titles()
     insert_job_titles(job_titles)
 
-    print("Job titles have been inserted into the database.")
+    print("Job titles have been processed and inserted into the database.")
